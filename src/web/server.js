3ddc47197c062da227f11baa10ab84c05eebf6f9
DuckySoLucky <75372052+DuckySoLucky@users.noreply.github.com>
2022-05-16 13:13:01 +0200

    Update server.js

--- src/web/server.js
+++ src/web/server.js
@@ -1,15 +1,12 @@
-var http = require('http');
-var fs = require('fs');
-
-const PORT=3000; 
-
-fs.readFile('./src/web/index.html', function (err, html) {
-    if (err) throw err;    
-    http.createServer(function(request, response) {  
-        response.writeHeader(200, {"Content-Type": "text/html"});  
-        response.write(html);  
-        response.end();  
-    }).listen(PORT);
-});
+const express = require('express');
+const server = express();
  
-
+server.all('/', (req, res) => {
+  res.send(`Bot is Online!`)
+})
+ 
+function keepAlive() {
+  server.listen(1382, () => { console.log("Server is Ready!!") });
+}
+ 
+module.exports = keepAlive;
