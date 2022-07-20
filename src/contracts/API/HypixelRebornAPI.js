a6c6dc3bda704107bfe9bb4a7f924a65e9e84fb8
DuckySoLucky <robertkovac160@gmail.com>
2022-07-20 20:31:53 +0200

    2.0

--- src/contracts/API/HypixelRebornAPI.js
+++
@@ -1,6 +0,0 @@
-const HypixelAPIReborn = require('hypixel-api-reborn');
-const config = require('../../../config.json')
-
-const hypixel = new HypixelAPIReborn.Client(config.api.hypixelAPIkey, {cache: true});
-
-module.exports = hypixel;
--- src/contracts/Hypixel.js
+++
@@ -1,4 +0,0 @@
-const HypixelAPIReborn = require('hypixel-api-reborn');
-const config = require('../../config.json')
-const hypixel = new HypixelAPIReborn.Client(config.minecraft.apiKey, {cache: true});
-module.exports = hypixel;
