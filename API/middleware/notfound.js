85c86b66aa41d95031ea00043ad76afb6cfd0270
DuckySoLucky <robertkovac160@gmail.com>
2022-08-15 10:57:55 +0200

    Cleaned API code

--- API/middleware/notfound.js
+++
@@ -1,7 +0,0 @@
-//CREDIT: https://github.com/Senither/hypixel-skyblock-facade
-module.exports = (_, response) => {
-  return response.status(404).json({
-    status: 404,
-    reason: 'Route not found',
-  })
-}
