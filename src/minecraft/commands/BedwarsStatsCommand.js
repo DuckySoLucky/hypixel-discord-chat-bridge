7295eae083de6e94448b248a471a151bd3d1c485
Kath <55346310+Kathund@users.noreply.github.com>
2022-10-28 21:02:01 +0800

    Update BedwarsStatsCommand.js

--- src/minecraft/commands/BedwarsStatsCommand.js
+++ src/minecraft/commands/BedwarsStatsCommand.js
@@ -18,7 +18,7 @@ class bedwarsCommand extends MinecraftCommand {
       let msg = this.getArgs(message);
       if(msg[0]) username = msg[0]
       const player = await hypixel.getPlayer(username)
-      this.send(`/gc [BW] [${player.stats.bedwars.level}✫] ${player.nickname}ᐧᐧᐧᐧFK:${addCommas(player.stats.bedwars.finalKills)}ᐧᐧᐧᐧFKDR:${player.stats.bedwars.finalKDRatio}ᐧᐧᐧᐧWLR:${player.stats.bedwars.WLRatio}ᐧᐧᐧᐧWS:${player.stats.bedwars.winstreak}`)
+      this.send(`/gc [BW] [${player.stats.bedwars.level}✫] ${player.nickname} FK: ${addCommas(player.stats.bedwars.finalKills)} FKDR: ${player.stats.bedwars.finalKDRatio} Wins: ${player.stats.bedwars.wins} WLR: ${player.stats.bedwars.WLRatio} Beds: ${player.stats.bedwars.beds.broken} BLR: ${player.stats.bedwars.beds.BLRatio} WS: ${player.stats.bedwars.winstreak}`)
     } catch (error) {
       console.log(error)
       this.send('There is no player with the given UUID or name or player has never joined Hypixel.')
