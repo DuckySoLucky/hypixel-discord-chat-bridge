dfcfd994c85014b51ee2bd2878a6db9355e85654
Kath <55346310+Kathund@users.noreply.github.com>
2022-10-28 22:05:47 +0800

    Update BedwarsStatsCommand.js

--- src/minecraft/commands/BedwarsStatsCommand.js
+++ src/minecraft/commands/BedwarsStatsCommand.js
@@ -16,9 +16,20 @@ class bedwarsCommand extends MinecraftCommand {
   async onCommand(username, message) {
     try {
       let msg = this.getArgs(message);
-      if(msg[0]) username = msg[0]
+      if (msg[0]) username = msg[0];
+      // check message for second part of command for mode
+      let mode = msg[1] ? msg[1] : 'all';
+      console.log(mode)
       const player = await hypixel.getPlayer(username)
-      this.send(`/gc [BW] [${player.stats.bedwars.level}✫] ${player.nickname} FK: ${addCommas(player.stats.bedwars.finalKills)} FKDR: ${player.stats.bedwars.finalKDRatio} Wins: ${player.stats.bedwars.wins} WLR: ${player.stats.bedwars.WLRatio} Beds: ${player.stats.bedwars.beds.broken} BLR: ${player.stats.bedwars.beds.BLRatio} WS: ${player.stats.bedwars.winstreak}`)
+      if (mode == 'solo' || mode == 'doubles' || mode == 'threes' || mode == 'fours' || mode == '4v4') {
+        this.send(`/gc [BW] [${player.stats.bedwars.level}✫] ${player.nickname} ${mode} FK: ${addCommas(player.stats.bedwars[mode].finalKills)} FKDR: ${player.stats.bedwars[mode].finalKDRatio} Wins: ${player.stats.bedwars[mode].wins} WLR: ${player.stats.bedwars[mode].WLRatio} Beds: ${player.stats.bedwars[mode].beds.broken} BLR: ${player.stats.bedwars[mode].beds.BLRatio} WS: ${player.stats.bedwars[mode].winstreak}`)
+      }
+      else if (mode == 'all' || mode == 'overall' || mode == null) {
+        this.send(`/gc [BW] [${player.stats.bedwars.level}✫] ${player.nickname} FK: ${addCommas(player.stats.bedwars.finalKills)} FKDR: ${player.stats.bedwars.finalKDRatio} Wins: ${player.stats.bedwars.wins} WLR: ${player.stats.bedwars.WLRatio} Beds: ${player.stats.bedwars.beds.broken} BLR: ${player.stats.bedwars.beds.BLRatio} WS: ${player.stats.bedwars.winstreak}`)
+      }
+      else {
+        this.send(`/gc [BW] [${player.stats.bedwars.level}✫] ${player.nickname} FK: ${addCommas(player.stats.bedwars.finalKills)} FKDR: ${player.stats.bedwars.finalKDRatio} Wins: ${player.stats.bedwars.wins} WLR: ${player.stats.bedwars.WLRatio} Beds: ${player.stats.bedwars.beds.broken} BLR: ${player.stats.bedwars.beds.BLRatio} WS: ${player.stats.bedwars.winstreak}`)
+      }
     } catch (error) {
       console.log(error)
       this.send('There is no player with the given UUID or name or player has never joined Hypixel.')
