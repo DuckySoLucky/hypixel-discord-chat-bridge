f9466e2c5d8c88fea88297ef6b24d8002b8c2229
Kath <55346310+Kathund@users.noreply.github.com>
2022-10-29 19:47:35 +0800

    Revert "Update DenickerCommand.js"
    This reverts commit 505d5e17ff5a82aa21958b7523c372d420bd088b.

--- src/minecraft/commands/DenickerCommand.js
+++ src/minecraft/commands/DenickerCommand.js
@@ -15,18 +15,17 @@ class DenickerCommand extends MinecraftCommand {
     }
 
     async onCommand(username, message) {
-        // try {
-        //     username = this.getArgs(message)[0];
-        //     const response = (await axios.get(`${config.api.antiSniperAPI}/denick?key=${config.api.antiSniperKey}&nick=${username}`)).data;
+        try {
+            username = this.getArgs(message)[0];
+            const response = (await axios.get(`${config.api.antiSniperAPI}/denick?key=${config.api.antiSniperKey}&nick=${username}`)).data;
 
-        //     if (!response.player?.ign) return this.send('/gc Sorry, I wasn\'t able to denick this person.')
+            if (!response.player?.ign) return this.send('/gc Sorry, I wasn\'t able to denick this person.')
 
-        //     const player = await hypixel.getPlayer(response.player?.ign);
-        //     this.send(`/gc ${player.rank ? `[${player.rank}] ` : ``}${response.player?.ign} is nicked as ${response.player.queried_nick}`);
-        // } catch (error) {
-        //     this.send('/gc Sorry, I wasn\'t able to denick this person.')
-        // }
-        this.send(`/gc Denicking has been patched`)
+            const player = await hypixel.getPlayer(response.player?.ign);
+            this.send(`/gc ${player.rank ? `[${player.rank}] ` : ``}${response.player?.ign} is nicked as ${response.player.queried_nick}`);
+        } catch (error) {
+            this.send('/gc Sorry, I wasn\'t able to denick this person.')
+        }
     }
 }
 
