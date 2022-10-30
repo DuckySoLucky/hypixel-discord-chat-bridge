50a7bd9fd40b78e931e7e5a98db0c8dd8e2c4b3d
Kath <55346310+Kathund@users.noreply.github.com>
2022-10-30 16:00:00 +0800

    Update dailyCommand.js

--- src/minecraft/commands/dailyCommand.js
+++ src/minecraft/commands/dailyCommand.js
@@ -6,20 +6,20 @@ const fetch = (...args) => import('node-fetch').then(({
 
 function Formatter(num, digits) {
     const lookup = [
-      { value: 1, symbol: "" },
-      { value: 1e3, symbol: "k" },
-      { value: 1e6, symbol: "m" },
-      { value: 1e9, symbol: "b" },
-      { value: 1e12, symbol: "t" },
-      { value: 1e15, symbol: "q" },
-      { value: 1e18, symbol: "qi" }
+        { value: 1, symbol: "" },
+        { value: 1e3, symbol: "k" },
+        { value: 1e6, symbol: "m" },
+        { value: 1e9, symbol: "b" },
+        { value: 1e12, symbol: "t" },
+        { value: 1e15, symbol: "q" },
+        { value: 1e18, symbol: "qi" }
     ];
     const rx = /\.0+$|(\.[0-9]*[1-9])0+$/;
-    var item = lookup.slice().reverse().find(function(item) {
-      return num >= item.value;
+    var item = lookup.slice().reverse().find(function (item) {
+        return num >= item.value;
     });
     return item ? (num / item.value).toFixed(digits).replace(rx, "$1") + item.symbol : "0";
-  }
+}
 
 class dailyCommand extends MinecraftCommand {
     constructor(minecraft) {
