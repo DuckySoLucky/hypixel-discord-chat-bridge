0b772ec70341f0b264bdfa15f83537896fcc7430
Kath <55346310+Kathund@users.noreply.github.com>
2022-11-01 19:04:19 +0800

    Update BedwarsStatsCommand.js

--- src/minecraft/commands/BedwarsStatsCommand.js
+++ src/minecraft/commands/BedwarsStatsCommand.js
@@ -16,23 +16,22 @@ class bedwarsCommand extends MinecraftCommand {
   async onCommand(username, message) {
     try {
       let msg = this.getArgs(message);
-      let mode = null;
-
-      if (msg[0] && !['solo', 'doubles', 'threes', 'fours', '4v4'].includes(msg[0])) {
-        username = msg[0]
-        if (msg[1]) mode = msg[1];
+      let ign = msg[0] ? msg[0] : username;
+      if (ign == 'solo' || ign == 'doubles' || ign == 'threes' || ign == 'fours' || ign == '4v4') {
+        const player = await hypixel.getPlayer(username)
+        this.send(`/gc [BW] [${player.stats.bedwars.level}✫] ${player.nickname} ${ign} FK: ${addCommas(player.stats.bedwars[ign].finalKills)} FKDR: ${player.stats.bedwars[ign].finalKDRatio} Wins: ${player.stats.bedwars[ign].wins} WLR: ${player.stats.bedwars[ign].WLRatio} Beds: ${player.stats.bedwars[ign].beds.broken} BLR: ${player.stats.bedwars[ign].beds.BLRatio} WS: ${player.stats.bedwars[ign].winstreak}`)
       }
-
-      const player = await hypixel.getPlayer(username)
-
-      if (!['solo', 'doubles', 'threes', 'fours', '4v4'].includes(username)) {
-        this.send(`/gc [BW] [${player.stats.bedwars.level}✫] ${player.nickname} ${username} FK: ${addCommas(player.stats.bedwars[username].finalKills)} FKDR: ${player.stats.bedwars[username].finalKDRatio} Wins: ${player.stats.bedwars[username].wins} WLR: ${player.stats.bedwars[username].WLRatio} Beds: ${player.stats.bedwars[username].beds.broken} BLR: ${player.stats.bedwars[username].beds.BLRatio} WS: ${player.stats.bedwars[username].winstreak}`)
-      
-      } else {
-        if (['solo', 'doubles', 'threes', 'fours', '4v4'].includes(mode)) {
+      else {
+        // check message for second part of command for mode
+        let mode = msg[1] ? msg[1] : 'all';
+        const player = await hypixel.getPlayer(ign)
+        if (mode == 'solo' || mode == 'doubles' || mode == 'threes' || mode == 'fours' || mode == '4v4') {
           this.send(`/gc [BW] [${player.stats.bedwars.level}✫] ${player.nickname} ${mode} FK: ${addCommas(player.stats.bedwars[mode].finalKills)} FKDR: ${player.stats.bedwars[mode].finalKDRatio} Wins: ${player.stats.bedwars[mode].wins} WLR: ${player.stats.bedwars[mode].WLRatio} Beds: ${player.stats.bedwars[mode].beds.broken} BLR: ${player.stats.bedwars[mode].beds.BLRatio} WS: ${player.stats.bedwars[mode].winstreak}`)
-        
-        } else {
+        }
+        else if (mode == 'all' || mode == 'overall' || mode == null) {
+          this.send(`/gc [BW] [${player.stats.bedwars.level}✫] ${player.nickname} FK: ${addCommas(player.stats.bedwars.finalKills)} FKDR: ${player.stats.bedwars.finalKDRatio} Wins: ${player.stats.bedwars.wins} WLR: ${player.stats.bedwars.WLRatio} Beds: ${player.stats.bedwars.beds.broken} BLR: ${player.stats.bedwars.beds.BLRatio} WS: ${player.stats.bedwars.winstreak}`)
+        }
+        else {
           this.send(`/gc [BW] [${player.stats.bedwars.level}✫] ${player.nickname} FK: ${addCommas(player.stats.bedwars.finalKills)} FKDR: ${player.stats.bedwars.finalKDRatio} Wins: ${player.stats.bedwars.wins} WLR: ${player.stats.bedwars.WLRatio} Beds: ${player.stats.bedwars.beds.broken} BLR: ${player.stats.bedwars.beds.BLRatio} WS: ${player.stats.bedwars.winstreak}`)
         }
       }
