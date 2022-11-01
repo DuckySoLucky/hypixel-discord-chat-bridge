78b2c236fde6a0170b3e820f2cee70acc4553c26
Kath <55346310+Kathund@users.noreply.github.com>
2022-11-01 20:03:21 +0800

    Update monthlyCommand.js

--- src/minecraft/commands/monthlyCommand.js
+++ src/minecraft/commands/monthlyCommand.js
@@ -4,23 +4,6 @@ const fetch = (...args) => import('node-fetch').then(({
     default: fetch
 }) => fetch(...args)).catch(err => console.log(err));
 
-function Formatter(num, digits) {
-    const lookup = [
-      { value: 1, symbol: "" },
-      { value: 1e3, symbol: "k" },
-      { value: 1e6, symbol: "m" },
-      { value: 1e9, symbol: "b" },
-      { value: 1e12, symbol: "t" },
-      { value: 1e15, symbol: "q" },
-      { value: 1e18, symbol: "qi" }
-    ];
-    const rx = /\.0+$|(\.[0-9]*[1-9])0+$/;
-    var item = lookup.slice().reverse().find(function(item) {
-      return num >= item.value;
-    });
-    return item ? (num / item.value).toFixed(digits).replace(rx, "$1") + item.symbol : "0";
-  }
-
 class dailyCommand extends MinecraftCommand {
     constructor(minecraft) {
         super(minecraft)
