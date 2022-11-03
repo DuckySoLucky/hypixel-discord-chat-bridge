737aaab15758091ac70ac7ed71a78ca4c0ff0e02
kathund <jacobbray2007@gmail.com>
2022-11-03 14:17:39 +0800

    Update playerCommand.js

--- src/minecraft/commands/playerCommand.js
+++ src/minecraft/commands/playerCommand.js
@@ -10,7 +10,7 @@ function Formatter(num, digits) {
     { value: 1e12, symbol: "t" }
   ];
   const rx = /.0+$|(.[0-9]*[1-9])0+$/;
-  var item = lookup.slice().reverse().find(function(item) {
+  var item = lookup.slice().reverse().find(function (item) {
     return num >= item.value;
   });
   return item ? (num / item.value).toFixed(digits).replace(rx, "$1") + item.symbol : "0";
@@ -25,18 +25,18 @@ class PlayerCommand extends MinecraftCommand {
     this.description = 'Look up a player'
   }
 
- 
-onCommand(username, message) {
+
+  onCommand(username, message) {
     const player = message.split(' ')[1]
-      hypixel.getPlayer(player).then((data) => {
-        var ap = Formatter(((data.achievementPoints).toFixed(0)), 2)
-        var karma = Formatter(((data.karma).toFixed(0)), 2)
-          this.send(`/gc Info for [${player.rank}] ${player} - Karma: ${karma} Network Level: ${data.level} Achievement Points: ${ap} Guild: ${data.guild} Online Status: ${data.isOnline}`)
-      }).catch((error) => {
-        this.send(`/gc ${player} is not a valid player!`)
-        console.log(error)
-      })
-    }
+    hypixel.getPlayer(player).then((data) => {
+      var ap = Formatter(((data.achievementPoints).toFixed(0)), 2)
+      var karma = Formatter(((data.karma).toFixed(0)), 2)
+      this.send(`/gc Info for [${player.rank}] ${player} - Karma: ${karma} Network Level: ${data.level} Achievement Points: ${ap} Guild: ${data.guild} Online Status: ${data.isOnline}`)
+    }).catch((error) => {
+      this.send(`/gc ${player} is not a valid player!`)
+      console.log(error)
+    })
   }
+}
 
 module.exports = PlayerCommand
