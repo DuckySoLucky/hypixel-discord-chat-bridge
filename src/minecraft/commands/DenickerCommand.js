f7bea1c7bef602a89bf2ade1210a26359d956fd3
kathund <jacobbray2007@gmail.com>
2022-11-03 12:13:54 +0800

    Update DenickerCommand.js

--- src/minecraft/commands/DenickerCommand.js
+++ src/minecraft/commands/DenickerCommand.js
@@ -17,12 +17,22 @@ class DenickerCommand extends MinecraftCommand {
     async onCommand(username, message) {
         try {
             username = this.getArgs(message)[0];
+            let mode = msg[1] ? msg[1] : 'bedwars';
             const response = (await axios.get(`${config.api.antiSniperAPI}/denick?key=${config.api.antiSniperKey}&nick=${username}`)).data;
-
-            if (!response.player?.ign) return this.send('/gc Sorry, I wasn\'t able to denick this person.')
-
-            const player = await hypixel.getPlayer(response.player?.ign);
-            this.send(`/gc ${player.rank ? `[${player.rank}] ` : ``}${response.player?.ign} is nicked as ${response.player.queried_nick}`);
+            if (!response.player?.ign) {
+                this.send('/gc Sorry, I wasn\'t able to denick this person.')
+            } else {
+                const player = await hypixel.getPlayer(response.player?.ign);
+                if (mode == 'duels') {
+                    this.send(`/gc ${player.rank ? `[${player.rank}] ` : ``}${response.player?.ign} is nicked as ${response.player.queried_nick} | [${player.stats.duels.division}] ${username} Wins: ${player.stats.duels.wins} | Current WS: ${player.stats.duels.winstreak} | Best WS: ${player.stats.duels.bestWinstreak} | WLR: ${player.stats.duels.WLRatio}`);
+                }
+                else if (mode == 'bedwars' ) {
+                    this.send(`/gc ${player.rank ? `[${player.rank}] ` : ``}${response.player?.ign} is nicked as ${response.player.queried_nick} | [${player.stats.bedwars.level}✫] ${player.nickname} FK: ${addCommas(player.stats.bedwars.finalKills)} FKDR: ${player.stats.bedwars.finalKDRatio} Wins: ${player.stats.bedwars.wins} WLR: ${player.stats.bedwars.WLRatio} Beds: ${player.stats.bedwars.beds.broken} BLR: ${player.stats.bedwars.beds.BLRatio} WS: ${player.stats.bedwars.winstreak}`);
+                }
+                else {
+                    this.send(`/gc ${player.rank ? `[${player.rank}] ` : ``}${response.player?.ign} is nicked as ${response.player.queried_nick} | [${player.stats.bedwars.level}✫] ${player.nickname} FK: ${addCommas(player.stats.bedwars.finalKills)} FKDR: ${player.stats.bedwars.finalKDRatio} Wins: ${player.stats.bedwars.wins} WLR: ${player.stats.bedwars.WLRatio} Beds: ${player.stats.bedwars.beds.broken} BLR: ${player.stats.bedwars.beds.BLRatio} WS: ${player.stats.bedwars.winstreak}`);
+                }
+            }
         } catch (error) {
             this.send('/gc Sorry, I wasn\'t able to denick this person.')
         }
