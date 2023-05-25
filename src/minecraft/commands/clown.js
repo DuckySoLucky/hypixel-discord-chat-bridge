319a932fe7b4a1498feeb4f8477db75fcb624b59
Kurumi78 <30587387+Kurumi78@users.noreply.github.com>
2023-05-24 20:55:13 -0500

    Update clown.js
    clown

--- src/minecraft/commands/clown.js
+++ src/minecraft/commands/clown.js
@@ -1,1 +1,43 @@
 
+const minecraftCommand = require("../../contracts/minecraftCommand.js");
+const hypixel = require("../../contracts/API/HypixelRebornAPI.js");
+const config = require("../../../config.json");
+const axios = require("axios");
+
+class clown extends minecraftCommand {
+  constructor(minecraft) {
+    super(minecraft);
+
+    this.name = "clown";
+    this.aliases = [bonzo, bonzomask];
+    this.description = "Bonzo Mask.";
+    this.options = [];
+  }
+
+  async onCommand(username, message) {
+    try {
+        return this.send("/gc Denick has been disabled by the Bot's Admin.");
+        /*
+      username = this.getArgs(message)[0];
+      const { data } = await axios.get(
+        `${config.minecraft.API.antiSniperAPI}/v2/other/denick?key=${config.minecraft.API.antiSniperKey}&nick=${username}`
+      );
+
+      if (data.player?.ign == null) {
+        return this.send("/gc Sorry, I wasn't able to denick this person.");
+      }
+
+      const player = await hypixel.getPlayer(data.player?.ign);
+
+      this.send(
+        `/gc ${player.rank ? `[${player.rank}] ` : ``}${
+          data.player?.ign
+        } is nicked as ${data.player.queried_nick}`
+      ); */
+    } catch (error) {
+      this.send(`/gc Error: ${error?.response?.data?.error || error}`);
+    }
+  }
+}
+
+module.exports = clown;
