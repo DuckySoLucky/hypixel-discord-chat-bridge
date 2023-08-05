874a9374c28604c20048b87ab5a103425cdcfe6b
Kurumi78 <30587387+Kurumi78@users.noreply.github.com>
2023-08-05 12:51:37 -0500

    Update clown.js

--- src/minecraft/commands/clown.js
+++ src/minecraft/commands/clown.js
@@ -9,31 +9,15 @@ class clown extends minecraftCommand {
     super(minecraft);
 
     this.name = "clown";
-    this.aliases = [bonzo, bonzomask];
+    this.aliases = ["bonzo", "bonzomask", "bozo", "bozomask"];
     this.description = "Bonzo Mask.";
     this.options = [];
   }
 
   async onCommand(username, message) {
     try {
-        return this.send("/gc Denick has been disabled by the Bot's Admin.");
-        /*
-      username = this.getArgs(message)[0];
-      const { data } = await axios.get(
-        `${config.minecraft.API.antiSniperAPI}/v2/other/denick?key=${config.minecraft.API.antiSniperKey}&nick=${username}`
-      );
+        return this.send("/gc https://i.imgur.com/dH4Vq3H.png");
 
-      if (data.player?.ign == null) {
-        return this.send("/gc Sorry, I wasn't able to denick this person.");
-      }
-
-      const player = await hypixel.getPlayer(data.player?.ign);
-
-      this.send(
-        `/gc ${player.rank ? `[${player.rank}] ` : ``}${
-          data.player?.ign
-        } is nicked as ${data.player.queried_nick}`
-      ); */
     } catch (error) {
       this.send(`/gc Error: ${error?.response?.data?.error || error}`);
     }
