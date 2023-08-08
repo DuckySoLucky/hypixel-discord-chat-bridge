2cfd06e8866ed65d23f2c800f07bf4fc6fc6b2c6
blahai <github@elissa.gay>
2023-08-08 16:34:04 +0300

    add boop command

---
+++ src/discord/commands/boop.js
@@ -0,0 +1,39 @@
+const HypixelDiscordChatBridgeError = require("../../contracts/errorHandler.js");
+const { EmbedBuilder } = require("discord.js");
+const config = require("../../../config.json");
+
+module.exports = {
+  name: "boop",
+  description: "Boops the given user.",
+  options: [
+    {
+      name: "name",
+      description: "Minecraft Username",
+      type: 3,
+      required: true,
+    },
+  ],
+
+  execute: async (interaction) => {
+    const user = interaction.member;
+    if (user.roles.cache.has(config.discord.roles.modRole) === false) {
+      throw new HypixelDiscordChatBridgeError("You do not have permission to use this command.");
+    }
+
+    const name = interaction.options.getString("name");
+    bot.chat(`/boop ${name}`);
+
+    const embed = new EmbedBuilder()
+      .setColor(5763719)
+      .setAuthor({ name: "Boop" })
+      .setDescription(`Successfully executed \`/boop ${name}\``)
+      .setFooter({
+        text: `by @duckysolucky | /help [command] for more information`,
+        iconURL: "https://imgur.com/tgwQJTX.png",
+      });
+
+    await interaction.followUp({
+      embeds: [embed],
+    });
+  },
+};
