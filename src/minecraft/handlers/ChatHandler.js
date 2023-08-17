aeaecf002dd79125972f22893999be4475bfc9ce
blahai <github@elissa.gay>
2023-08-17 15:57:41 +0300

    deprecate fragbot code

--- src/minecraft/handlers/ChatHandler.js
+++ src/minecraft/handlers/ChatHandler.js
@@ -44,36 +44,6 @@ class StateHandler extends eventHandler {
       return bot.chat("\u00a7");
     }
 
-    if (this.isPartyMessage(message) && config.minecraft.fragBot.enabled === true) {
-      const username = message.substr(54).startsWith("[")
-        ? message.substr(54).split(" ")[1].trim()
-        : message.substr(54).split(" ")[0].trim();
-
-      const { blacklist, blacklisted, whitelist, whitelisted } = config.minecraft.fragBot;
-      if (blacklist || whitelist) {
-        const uuid = await getUUID(username);
-
-        if (config.minecraft.fragBot.blacklist === true) {
-          if (blacklisted.includes(username) || blacklisted.includes(uuid)) {
-            return;
-          }
-        }
-
-        const members = await hypixel
-          .getGuild("player", bot.username)
-          .then(async (guild) => guild.members.map((member) => member.uuid));
-        if ((config.minecraft.fragBot.whitelist && whitelisted.includes(username)) || members.includes(uuid)) {
-          this.send(`/party accept ${username}`);
-          await delay(Math.floor(Math.random() * (6900 - 4200 + 1)) + 4200);
-          this.send(`/party leave`);
-        }
-      } else {
-        this.send(`/party accept ${username}`);
-        await delay(Math.floor(Math.random() * (6900 - 4200 + 1)) + 4200);
-        this.send(`/party leave`);
-      }
-    }
-
     if (this.isRequestMessage(message)) {
       const username = replaceAllRanks(
         message.split("has")[0].replaceAll("-----------------------------------------------------\n", "")
