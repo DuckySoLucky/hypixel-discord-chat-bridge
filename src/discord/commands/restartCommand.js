f7ca3a084cf4241d869357d41a4c083b8d958d4a
George <95067901+GeorgeFilos@users.noreply.github.com>
2023-08-28 21:19:48 +0300

    Add files via upload

---
+++ src/discord/commands/restartCommand.js
@@ -0,0 +1,39 @@
+const app = require("./../../Application.js");
+const config = require("./../../../config.json")
+
+module.exports = {
+    name: "restart",
+    description: "Restarts the ingame and discord bot.",
+  
+    execute: async (interaction) => {
+        if (config.discord.commands.checkPerms === true && interaction.member.roles.cache.has(config.discord.commands.commandRole) === false) {
+            throw new HypixelDiscordChatBridgeError("You do not have permission to use this command.");
+          }
+
+          const restartEmbed = new EmbedBuilder()
+          .setColor(0x0099ff)
+          .setTitle("⭕Restarting!")
+          .setDescription(`**Bot will restart ingame and on discord.`)
+          .setFooter({
+            text: `by @duckysolucky | /help [command] for more information`,
+            iconURL: "https://imgur.com/tgwQJTX.png",
+          });
+    
+        interaction.followUp({ embeds: [restartEmbed] });
+      
+          await bot.quit('restart')
+          await bot.end('restart')
+          await client.destroy()
+
+        app
+            .register()
+            .then(() => {
+            app.connect();
+            })
+              .catch((error) => {
+            console.error(error);
+             });
+
+    },
+  };
+  
