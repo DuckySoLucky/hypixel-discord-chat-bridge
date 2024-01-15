c43da32286f56b662f4d8fffb9990292039269f2
Kath <55346310+Kathund@users.noreply.github.com>
2024-01-15 14:43:31 +0800

    Raccon command

---
+++ src/minecraft/commands/RaccoonCommand.js
@@ -0,0 +1,37 @@
+const minecraftCommand = require("../../contracts/minecraftCommand.js");
+const { uploadImage } = require("../../contracts/API/imgurAPI.js");
+const axios = require("axios");
+
+class RaccoonCommand extends minecraftCommand {
+  constructor(minecraft) {
+    super(minecraft);
+
+    this.name = "raccoon";
+    this.aliases = ["raccn", "soupy", "soupyraccn"];
+    this.description = "Random image of a raccoon.";
+    this.options = [];
+  }
+
+  async onCommand(username, message) {
+    try {
+      const { data, status } = await axios.get(`https://raccoon.kath.lol/raccoon`);
+      if (status !== 200) {
+        // eslint-disable-next-line no-throw-literal
+        throw "An error occured while fetching the image. Please try again later.";
+      }
+
+      if (data === undefined) {
+        // eslint-disable-next-line no-throw-literal
+        throw "An error occured while fetching the image. Please try again later.";
+      }
+
+      const upload = await uploadImage(data.url);
+
+      this.send(`/gc Funny Raccoon: ${upload.data.link}`);
+    } catch (error) {
+      this.send(`/gc [ERROR] ${error ?? "Something went wrong.."}`);
+    }
+  }
+}
+
+module.exports = RaccoonCommand;
