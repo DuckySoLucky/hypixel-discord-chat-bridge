38446f2f96aea8bb921422355324dc85ca05874f
George <95067901+GeorgeFilos@users.noreply.github.com>
2024-01-19 18:31:18 +0200

    Update bestiary.js

--- API/stats/bestiary.js
+++ API/stats/bestiary.js
@@ -41,7 +41,7 @@ function getBestiary(userProfile) {
 
       if (category === "fishing") {
         for (const [key, value] of Object.entries(data)) {
-          if (key === "name" === "hasSubcategories") continue;
+          if (key === "name") continue;
 
           output[category][key] = {
             name: value.name,
