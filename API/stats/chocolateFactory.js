5c9ec48156caf3836c6c250ce552868846338188
Kath <admin@kath.lol>
2024-05-10 14:16:45 +0800

    feat(chocolateFactory)

---
+++ API/stats/chocolateFactory.js
@@ -0,0 +1,22 @@
+module.exports = (profile) => {
+  try {
+    return {
+      chocolate: {
+        current: profile.events.easter.chocolate || 0,
+        sincePrestige: profile.events.easter.chocolate_since_prestige || 0,
+        total: profile.events.easter.total_chocolate || 0,
+      },
+      employees: {
+        bro: profile.events.easter.employees.rabbit_bro || 0,
+        cousin: profile.events.easter.employees.rabbit_cousin || 0,
+        sis: profile.events.easter.employees.rabbit_sis || 0,
+        father: profile.events.easter.employees.rabbit_father || 0,
+        grandma: profile.events.easter.employees.rabbit_grandma || 0,
+      },
+      level: profile.events.easter.chocolate_level || 0,
+    };
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+};
