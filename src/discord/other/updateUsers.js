e903a2983c25468347d197a771f727efff17d26a
Kath <admin@kath.lol>
2024-07-18 08:37:13 +0800

    fix autoupdater oversite

--- src/discord/other/updateUsers.js
+++ src/discord/other/updateUsers.js
@@ -2,6 +2,6 @@ const updateRolesCommand = require("../commands/forceUpdateEveryone.js");
 const config = require("../../../config.json");
 const cron = require("node-cron");
 
-if (config.verification.autoUpdate) {
+if (config.verification.autoUpdater) {
   cron.schedule(`0 */${config.verification.autoUpdaterInterval} * * *`, () => updateRolesCommand.execute(null, true));
 }
