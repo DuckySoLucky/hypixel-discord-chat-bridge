26486c2b66c9fcc208b1b9ee617aedb9a6290602
Jacob <admin@kath.lol>
2025-02-17 17:20:24 +0800

    fix(eventNotifier): Event Notifier sending /gc at the start of the message

--- src/minecraft/other/eventNotifier.js
+++ src/minecraft/other/eventNotifier.js
@@ -38,12 +38,12 @@ if (config.minecraft.skyblockEventsNotifications.enabled) {
 
         const cTime = getCustomTime(customTime, event);
         if (cTime.length !== 0 && cTime.includes(minutes.toString())) {
-          eventBOT.send(`/gc [EVENT] ${eventData.name}${extraInfo}: Starting in ${minutes}m!`);
+          eventBOT.send(`[EVENT] ${eventData.name}${extraInfo}: Starting in ${minutes}m!`);
           await delay(1500);
         }
 
         if (minutes == 0) {
-          eventBOT.send(`/gc [EVENT] ${eventData.name}${extraInfo}: Starting now!`);
+          eventBOT.send(`[EVENT] ${eventData.name}${extraInfo}: Starting now!`);
           await delay(1500);
         }
       }
