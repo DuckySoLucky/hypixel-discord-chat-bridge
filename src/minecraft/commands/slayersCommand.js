4f5d781f7d4783312d6c35d0b321820009c7033f
DuckySoLucky <robertkovac160@gmail.com>
2025-02-28 18:21:45 +0100

    fix: slayer

--- src/minecraft/commands/slayersCommand.js
+++ src/minecraft/commands/slayersCommand.js
@@ -32,7 +32,7 @@ class SlayersCommand extends minecraftCommand {
   async onCommand(player, message) {
     try {
       const args = this.getArgs(message);
-      const slayer = ["zombie", "rev", "spider", "tara", "wolf", "sven", "eman", "enderman", "blaze", "demonlord", "vamp", "vampire"];
+      const slayer = ["zombie", "spider", "wolf", "enderman", "blaze", "vampire"];
 
       const slayerType = slayer.includes(args[1]) ? args[1] : null;
       player = args[0] || player;
@@ -47,13 +47,14 @@ class SlayersCommand extends minecraftCommand {
       if (slayerType) {
         this.send(`${username}'s ${titleCase(slayerType)} - ${slayerData[slayerType].level} Levels | Experience: ${formatNumber(slayerData[slayerType].xp)}`);
       } else {
-        const slayer = Object.keys(profile).reduce(
+        const slayer = Object.keys(slayerData).reduce(
           (acc, slayer) => `${acc} | ${titleCase(slayer)}: ${slayerData[slayer].level} (${formatNumber(slayerData[slayer].xp)})`,
           ""
         );
         this.send(`${username}'s Slayer: ${slayer.slice(3)}`);
       }
     } catch (error) {
+      console.log(error);
       this.send(`[ERROR] ${error}`);
     }
   }
