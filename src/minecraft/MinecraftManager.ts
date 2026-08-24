d20b249d1a1d58650fb2b300c07cdc292306c780
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2026-08-24 17:35:29 +0200

    semicolon!!

--- src/minecraft/MinecraftManager.ts
+++ src/minecraft/MinecraftManager.ts
@@ -196,7 +196,7 @@ class MinecraftManager extends CommunicationBridge {
       const chatMessage = this.prismarineChat.fromNotch(data.formattedMessage);
       const receivedMessage = chatMessage.toString();
 
-      const isAnyGuildMessage = this.messageHandler.isGuildMessage(receivedMessage) || this.messageHandler.isOfficerMessage(receivedMessage)
+      const isAnyGuildMessage = this.messageHandler.isGuildMessage(receivedMessage) || this.messageHandler.isOfficerMessage(receivedMessage);
 
       if (receivedMessage.trim().includes(message.trim()) && isAnyGuildMessage) {
         this.bot.removeListener("systemChat", messageListener);
